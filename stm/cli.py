27d2a4358b60a9cc8774ab481dad36d5b925bff3
Hart Traveller <hart@MBP.local>
2023-11-02 23:41:06 -0400

    stm/cli: update

---
+++ stm/cli.py
@@ -0,0 +1,10 @@
+import rich_click as click
+from stm.environment import PKG_VERSION
+
+
+@click.group(name="stm")
+@click.version_option(PKG_VERSION)
+@click.pass_context
+def entry():
+    # todo: cli message
+    ...
