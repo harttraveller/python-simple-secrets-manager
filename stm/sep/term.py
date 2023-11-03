348d3657249d22e130052e91c2f8fde9e2a74e0b
Hart Traveller <hart@MBP.local>
2023-11-03 00:12:17 -0400

    stm/sep/term: add vprint logic

--- stm/sep/term.py
+++ stm/sep/term.py
@@ -1,15 +1,32 @@
 import os
-from typing import Any, Union
+from typing import Any, Union, Optional
 from rich.console import Console
 from rich.color import ANSI_COLOR_NAMES
 
 # todo: move utils to separate package
 
 
-def vprint(obj: Union[str, Any], verbose: bool = True) -> None:
+def vprint(
+    obj: Union[str, Any],
+    color: Optional[str] = None,
+    verbose: bool = True,
+) -> None:
     "verbose print utility with rich formatting"
     if verbose:
-        Console().print(obj)
+        if isinstance(obj, str):
+            if color is None:
+                Console().print(obj)
+            else:
+                if color in ANSI_COLOR_NAMES.keys():
+                    Console().print(f"[{color}]{obj}[/{color}]")
+                else:
+                    for acn in ANSI_COLOR_NAMES.keys():
+                        Console().print(acn)
+                    raise ValueError(
+                        f"argument passed to 'color' must be one of the colors listed above"
+                    )
+        else:
+            Console().print(obj)
 
 
 def clear():
