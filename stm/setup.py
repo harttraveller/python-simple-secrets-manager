94aa52ac9ab38d660a798c90cf2954150c7d11ce
Hart Traveller <hart@MBP.local>
2023-11-03 01:44:49 -0400

    stm/setup: update

--- stm/setup.py
+++ stm/setup.py
@@ -1,9 +1,13 @@
+import os
 from stm.sep.io import save_toml
 from stm.env import PATH_STORE_DEFAULT, PATH_TOKENS_DEFAULT
 
 
-def token_storage():
+def token_storage(erase: bool = False):
     PATH_STORE_DEFAULT.mkdir(exist_ok=True)
 
+    if erase:
+        if PATH_TOKENS_DEFAULT.exists():
+            os.remove(PATH_TOKENS_DEFAULT)
     if not PATH_TOKENS_DEFAULT.exists():
-        save_toml(PATH_TOKENS_DEFAULT, dict())
+        save_toml(dict(), PATH_TOKENS_DEFAULT)
