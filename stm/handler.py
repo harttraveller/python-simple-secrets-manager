eaad774db9cf1412fbbaa89701be2cb1c1df866e
Hart Traveller <hart@MBP.local>
2023-11-02 23:45:32 -0400

    stm/handler: copy token handler from sunkcosts project

--- stm/handler.py
+++ stm/handler.py
@@ -1,8 +1,36 @@
 from stm.environment import PATH_STORE_DEFAULT
 
 
-class Tokens:
-    pass
+class TokenHandler:
+    @property
+    def data(self) -> dict:
+        return open_toml(FILE_CREDENTIALS)
+
+    @property
+    def tokens(self) -> list[str]:
+        return list(self.data.keys())
+
+    def exists(self, name: str) -> bool:
+        return name in self.tokens
+
+    def check(self, name) -> None:
+        if not self.exists(name):
+            raise ValueError("key does not exist")
+
+    def get(self, name: str) -> str:
+        self.check(name)
+        return self.data[name]
+
+    def save(self, name: str, key: str) -> None:
+        creds = self.data
+        creds[name] = key
+        save_toml(creds, FILE_CREDENTIALS)
+
+    def delete(self, name: str) -> None:
+        self.check(name)
+        creds = self.data
+        del creds[name]
+        save_toml(creds, FILE_CREDENTIALS)
 
 
-tokens = Tokens()
+tokens = TokenHandler()
