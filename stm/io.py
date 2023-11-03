01c0812da16def87089b602d98456606c0e070ba
Hart Traveller <hart@MBP.local>
2023-11-02 23:44:59 -0400

    stm/io: change typ hints to work with py 3.9

--- stm/io.py
+++ stm/io.py
@@ -1,15 +1,16 @@
 import toml
 from pathlib import Path
+from typing import Union
 
 
-def open_toml(path: str | Path) -> dict:
+def open_toml(path: Union[str, Path]) -> dict:
     with open(path) as toml_file:
         data = toml.loads(toml_file.read())
     toml_file.close()
     return data
 
 
-def save_toml(data: dict, path: str | Path) -> None:
+def save_toml(data: dict, path: Union[str, Path]) -> None:
     with open(path, "w") as toml_file:
         toml_file.write(toml.dumps(data))
     toml_file.close()
