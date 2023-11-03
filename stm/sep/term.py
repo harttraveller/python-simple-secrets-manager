86ec3beb6ebd2f8dcc9d975f87c18b08ba172825
Hart Traveller <hart@MBP.local>
2023-11-03 00:08:02 -0400

    stm/sep/term: add import, type hints

--- stm/sep/term.py
+++ stm/sep/term.py
@@ -1,12 +1,12 @@
 import os
-from typing import Any
+from typing import Any, Union
 from rich.console import Console
 from rich.color import ANSI_COLOR_NAMES
 
 # todo: move utils to separate package
 
 
-def vprint(obj: Any, verbose: bool = True) -> None:
+def vprint(obj: Union[str, Any], verbose: bool = True) -> None:
     "verbose print utility with rich formatting"
     if verbose:
         Console().print(obj)
