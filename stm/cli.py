2fd15a641c07640b31b186c1a51a2ff14b0ffca1
Hart Traveller <hart@MBP.local>
2023-11-02 23:53:24 -0400

    stm/cli: update

--- stm/cli.py
+++ stm/cli.py
@@ -3,7 +3,7 @@ from stm.handler import tokens
 from stm.environment import PKG_VERSION
 
 
-@click.group(name="stm")
+@click.group(name="tokens")
 @click.version_option(PKG_VERSION)
 @click.pass_context
 def entry():
