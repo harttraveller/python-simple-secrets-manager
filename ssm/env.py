8c45a00db5c2023f95b659ca2ebc00f3722f337d
Hart Traveller <hart@MBP.local>
2023-11-05 15:16:03 -0500

    ssm/env: change name

--- ssm/env.py
+++ ssm/env.py
@@ -4,5 +4,5 @@ from importlib import metadata
 PACKAGE_NAME = "simple-secrets-manager"
 PACKAGE_VERSION = metadata.version(PACKAGE_NAME)
 PATH_STORE_DEFAULT = Path.home() / ".secrets"
-PATH_SECRETS_DEFAULT = PATH_STORE_DEFAULT / "tokens.toml"
+PATH_SECRETS_DEFAULT = PATH_STORE_DEFAULT / "secrets.toml"
 # PATH_CONFIG_DEFAULT
