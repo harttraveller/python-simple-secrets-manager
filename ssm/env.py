8e31b6d6947431f6ab99b3a63c6e973ea5e77c6b
Hart Traveller <hart@MBP.local>
2023-11-05 15:11:50 -0500

    ssm/env: update vars

--- ssm/env.py
+++ ssm/env.py
@@ -1,9 +1,8 @@
 from pathlib import Path
 from importlib import metadata
 
-PKG_VERSION = metadata.version("simple-secrets-manager")
-
-
-PATH_STORE_DEFAULT = Path.home() / ".ssm"
+PACKAGE_NAME = "simple-secrets-manager"
+PACKAGE_VERSION = metadata.version(PACKAGE_NAME)
+PATH_STORE_DEFAULT = Path.home() / ".secrets"
 PATH_SECRETS_DEFAULT = PATH_STORE_DEFAULT / "tokens.toml"
 # PATH_CONFIG_DEFAULT
